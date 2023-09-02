513925f16a531368ee3b3a256ceb06f4a84f6bba
hamryt <rkdl0829@naver.com>
2023-09-02 15:25:43 +0900

    init

---
+++ src/main/kotlin/com/daehwa/brain/BrainApplication.kt
@@ -0,0 +1,11 @@
+package com.daehwa.brain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrainApplication
+
+fun main(args: Array<String>) {
+    runApplication<BrainApplication>(*args)
+}
---
+++ src/test/kotlin/com/daehwa/brain/BrainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.daehwa.brain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
